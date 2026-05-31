f85935c0368328061e595cc5e6f75e74df8c5b15
lorypelli <lorypelli@proton.me>
2026-05-31 12:37:44 +0200

    plugins for latex parsing

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -2,6 +2,7 @@ import { baseOptions } from '@/lib/layout.shared';
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { RootProvider } from 'fumadocs-ui/provider/next';
+import 'katex/dist/katex.min.css';
 import type { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export const docs = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+});
